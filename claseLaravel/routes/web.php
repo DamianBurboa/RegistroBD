f7b4f55838ab0c8812407e7a8adb17d9ad273037
unknown <you@example.com>
2021-02-22 00:28:57 -0700

    Registros

---
+++ claseLaravel/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('notas', function(){
+    $notas = DB::table('notas_tables')->get();
+
+    return view('notas', ['notas' => $notas]);
+})->name('notas.index');
+
+Route::get('agregar', function(){
+    return view('agregar');
+});
+
+Route::get('notas/{id}/editar', function ($id){
+    $notas = DB::Table('notas_tables')
+    ->where('id', $id)
+    ->first();
+
+    return 'Aqui se van a editar las notas' .$id;
+})->name('notas.edit');
